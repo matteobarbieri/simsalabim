07f10951ebb0030c8ad5f2bb3b8b929e5ae218a7
Matteo Barbieri <matteo.a.barbieri@gmail.com>
2023-07-22 18:22:04 +0000

    Add train commands for different sets of experiments

---
+++ scripts/train_command_aug_pretrain.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+###########################
+### All-purpose testing ###
+###########################
+
+EPOCHS=200
+
+DATASET=gtzan_augmented_256_x5
+MODEL_NAME=effnet_b1
+LR=5e-5
+LR_SCHEDULER_GAMMA=0.95
+ORIGINAL_ONLY_VAL=True
+BATCH_SIZE=16 # efficientnetb{0, 1}
+TAG=pretrain_aug_1024_focal
+PRETRAINED=True
+
+# DATASET=gtzan_augmented_256_x5
+# MODEL_NAME=effnet_b1
+# LR=5e-5
+# LR_SCHEDULER_GAMMA=0.95
+# ORIGINAL_ONLY_VAL=True
+# BATCH_SIZE=16 # efficientnetb{0, 1}
+# TAG=no_pretrain
+# PRETRAINED=False
+
+
+# FOLDS="fold_0" # For testing a single fold
+FOLDS="fold_0 fold_1 fold_2 fold_3 fold_4" # all 5 folds
+
+# Add model name and timestamp to tag
+TAG="${MODEL_NAME}_${TAG}"
+TAG="$(date +%Y%m%d_%H%M%S)_${TAG}"
+
+
+for fold in $FOLDS; do
+    poetry run \
+        python scripts/train.py with \
+            dataset=${DATASET} \
+            original_only_val=${ORIGINAL_ONLY_VAL} \
+            model_name=${MODEL_NAME} \
+            pretrained=${PRETRAINED} \
+            epochs=${EPOCHS} \
+            lr=${LR} \
+            tag=${TAG} \
+            lr_scheduler_gamma=${LR_SCHEDULER_GAMMA} \
+            batch_size=${BATCH_SIZE} \
+            test_subset=${fold}
+            
+done
